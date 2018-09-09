4e8d09f5d57ee57e4b3b04a5a26027162abf32c3
TakakiNishio <t2k4k0h61201@gmail.com>
2018-09-09 14:55:43 +0900

    [add] AizuSpiderVisionSensors project for ROS

---
+++ sample/WRS2018/T1-AizuSpiderVisionSensors-ROS.py
@@ -0,0 +1,4 @@
+import WRSUtil
+WRSUtil.loadProject(
+    "SingleSceneView", "T1", "AISTSimulator", "AizuSpiderVisionSensors",
+    enableVisionSimulation = True, targetVisionSensors = "FRONT_CAMERA", remoteType = "ROS")
