4f2731bded6331285c20e28b49c22dfc9edf8af6
Arnaud TANGUY <arn.tanguy@gmail.com>
2020-07-16 11:00:39 +0900

    Make VRML parser locale-free

--- src/Util/strtofloat.h
+++ src/Util/strtofloat.h
@@ -2,21 +2,14 @@
 // This is neccessary because the implementation of VC++6.0 uses 'strlen()' in the function,
 // so that it becomes too slow for a string buffer which has long length.
 
+#include <cctype>
+#include <cmath>
+
 namespace cnoid {
 
-#ifndef _MSC_VER
-
-#include <cstdlib>
-inline float strtof(const char* nptr, char** endptr){
-    return std::strtof(nptr, endptr);
-}
-inline double strtod(const char* nptr, char** endptr){
-    return std::strtod(nptr, endptr);
-}
-
-#else
-
-template<typename T> T strtofloat(const char* nptr, char** endptr)    
+// Replacement for 'strtod()' function
+// This is necessary for a locale-free version
+template<typename T> T strtofloat(const char* nptr, char** endptr)
 {
     const char* org = nptr;
     bool valid = false;
@@ -29,24 +22,24 @@ template<typename T> T strtofloat(const char* nptr, char** endptr)
         sign = -1.0;
         nptr++;
     }
-    if(isdigit((unsigned char)*nptr)){
+    if(std::isdigit((unsigned char)*nptr)){
         valid = true;
         do {
             value = value * 10.0 + (*nptr - '0');
             nptr++;
-        } while(isdigit((unsigned char)*nptr));
+        } while(std::isdigit((unsigned char)*nptr));
     }
     if(*nptr == '.'){
         //valid = false; // allow values which end with '.'. For example, "0."
         nptr++;
-        if(isdigit((unsigned char)*nptr)){
+        if(std::isdigit((unsigned char)*nptr)){
             T small = 0.1;
             valid = true;
             do {
                 value += small * (*nptr - '0');
                 small *= 0.1;
                 nptr++;
-            } while(isdigit((unsigned char)*nptr));
+            } while(std::isdigit((unsigned char)*nptr));
         }
     }
     if(valid && (*nptr == 'e' || *nptr == 'E')){
@@ -59,13 +52,13 @@ template<typename T> T strtofloat(const char* nptr, char** endptr)
             psign = -1.0;
             nptr++;
         }
-        if(isdigit((unsigned char)*nptr)){
+        if(std::isdigit((unsigned char)*nptr)){
             valid = true;
             T p = 0.0;
             do {
                 p = p * 10.0 + (*nptr - '0');
                 nptr++;
-            } while(isdigit((unsigned char)*nptr));
+            } while(std::isdigit((unsigned char)*nptr));
             value *= pow(10.0, (double)(psign * p));
         }
     }
@@ -82,6 +75,5 @@ inline float strtof(const char* nptr, char** endptr) {
 inline double strtod(const char* nptr, char** endptr) {
     return strtofloat<double>(nptr, endptr);
 }
-#endif
 
 }
